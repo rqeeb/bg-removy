cf8be7118f0f2eb96c45f050470863bae012b12f
RaqeebDev <raqeebauto@gmail.com>
2026-04-08 19:46:11 +0530

    Navbar done

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg'
-import logo_icon from './logo_icon.svg'
+import logo from "./logo.svg";
+import logo_icon from "./logo_icon.svg";
+import arrow_icon from "./arrow_icon.svg";
 
 export const assets = {
-    logo,
-    logo_icon,
-}
+  logo,
+  logo_icon,
+  arrow_icon,
+};
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,12 +3,11 @@ import { assets } from "../assets/assets";
 
 const Navbar = () => {
   return (
-    <div>
-      <img src={assets.logo} alt="logo" />
-      <button>Get started </button>
-      {/* TODO: Add arrow image in button ^^ */}
-
-      
+    <div className="flex items-center justify-between mx-4 py-3 lg:mx-44">
+      <img className="w-32 sm-44" src={assets.logo} alt="logo" />
+      <button className="bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 sm:px-8 sm:py-3 text-sm rounded-full ">
+        Get started <img className="w-3 sm:w-4  " src={assets.arrow_icon}  alt="" />{" "}
+      </button>
     </div>
   );
 };
